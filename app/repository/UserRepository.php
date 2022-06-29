ab04326565cb7a299c0eab2face1c3fd2cc88cd5
Maria Eduarda Heck <54290646+Mariaehsb@users.noreply.github.com>
2022-06-29 14:53:03 -0300

    login

--- app/repository/UserRepository.php
+++ app/repository/UserRepository.php
@@ -14,13 +14,25 @@
 
         public function create(UserModel $myuser) {
 
+            /* VALIDAÇÃO DE EMAIL (DEVERIA ESTAR AQUI) */
+            $query = "SELECT * FROM users WHERE email = :email";
+            $prepare = $this->conn->prepare($query);
+            $prepare->bindValue(":email", $myuser->getEmail());
+            $prepare->execute();
+
+            $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+
+            if(count($result) > 0) {
+                throw new Exception("Email já cadastrado");
+            }
+
             try {
 
-                $query = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)";
+                $query = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
                 $prepare = $this->conn->prepare($query);
-                $prepare->bindValue(":username", $user->getUsername());
-                $prepare->bindValue(":email", $user->getEmail());
-                $prepare->bindValue(":password", $user->getPassword());
+                $prepare->bindValue(":username", $myuser->getUsername());
+                $prepare->bindValue(":email", $myuser->getEmail());
+                $prepare->bindValue(":password", $myuser->getPassword());
                 $prepare->execute();
 
                 return $this->conn->lastInsertId();
@@ -33,17 +45,19 @@
         public function findAll(): array {
 
             $table = $this->conn->query("SELECT * FROM myusers");
-            $usuarios  = $table->fetchAll(PDO::FETCH_ASSOC);
+            //$usuarios  = $table->fetchAll(PDO::FETCH_ASSOC);
+            $usuarios  = $table->fetchAll(PDO::FETCH_CLASS, "UserModel");
+
 
             return $usuarios;
         }
 
-        public function findUserById(int $userId) {
+        public function findUserById(int $id) {
 
             $query = "SELECT * FROM users WHERE users.id = ?";
             
             $prepare = $this->conn->prepare($query);
-            $prepare->bindParam(1, $userId, PDO::PARAM_INT);
+            $prepare->bindParam(1, $id, PDO::PARAM_INT);
 
             if($prepare->execute()){
             
@@ -56,19 +70,34 @@
             return $usuario;
         }
 
+        public function findUserByEmail(string $email): UserModel  {
+
+            $query = "SELECT * FROM myusers WHERE users.email = ?";
+            
+            $prepare = $this->conn->prepare($query);
+            $prepare->bindParam(1, $email, PDO::PARAM_STR);
+
+            if($prepare->execute()){
+                $usuario  = $prepare->fetchObject("UserModel");
+            } else {
+                $usuario = null;
+            }
+
+            return $usuario;
+        }
+
         public function update(UserModel $myuser) {
 
-            $query = "UPDATE users SET username = :username, email = :email, password = :password WHERE id = :id";
+            $query = "UPDATE users SET name = :name, email = :email, password = :password WHERE id = :id";
             $prepare = $this->conn->prepare($query);
-            $prepare->bindValue(":username", $user->getUsername());
+            $prepare->bindValue(":name", $user->getUsername());
             $prepare->bindValue(":email", $user->getEmail());
             $prepare->bindValue(":password", $user->getPassword());
             $prepare->bindValue(":id", $user->getId());
             $prepare->execute();
         }
 
-        public function deleteById( int $id) {
-
+        public function deleteById( int $id) { 
             $query = "DELETE FROM users WHERE id = :id";
 
             $prepare = $this->conn->prepare($query);
