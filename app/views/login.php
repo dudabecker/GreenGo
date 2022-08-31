51a47e88f2d57ca374004a87e62f2dd66bbaa139
Duda Becker <54290661+dudabecker@users.noreply.github.com>
2022-08-31 14:08:49 -0300

    Update login.php

--- app/views/login.php
+++ app/views/login.php
@@ -49,7 +49,7 @@
                 </button>
 
                 <span class="trocar-form me-auto">
-                    Novo por aqui? <a href="cadastro.html" class="link-cadastro">Cadastrar</a>
+                    Novo por aqui? <a href="cadastro.php" class="link-cadastro">Cadastrar</a>
                 </span>
             </div>
 
