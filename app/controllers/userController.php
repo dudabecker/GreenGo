5de3f11a21afa290cc10c2f909cb39dc914277e2
Maria Eduarda Heck <54290646+Mariaehsb@users.noreply.github.com>
2022-09-14 17:43:04 -0300

    atualização heck REGISTRO FUNCIONANDO

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -1,25 +1,70 @@
 <?php
 
-// Exibe os erros em uma página web (DEVERIA ESTAR AQUI?).
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . "/../repository/UserRepository.php";
-require_once __DIR__ . "/Controller.php";
+//require_once __DIR__ . "/Controller.php";
 
-function create(){
+$userc = new ControllerUser();
 
-    $name         = $_POST['field_name'];
-    $genero       = $_POST['field_genero'];
-    $escolaridade = $_POST['field_escolaridade'];
-    $email        = $_POST['field_email'];
-    $password     = $_POST['field_password'];
+class ControllerUser{
+
+	function __construct(){
+		if(isset($_POST["action"])){
+			$action = $_POST["action"];
+		}else if(isset($_GET["action"])){
+			$action = $_GET["action"];
+		}
+		//print_r($action);
+
+		if(isset($action)){
+			$this->callAction($action);
+		}else{
+			$msg = "Nenhuma acao a ser processada...";
+            print_r($msg);
+			//include_once "index.php";
+		}
+	}
+
+    public function callAction(string $functionName = null){
+        //print_r($functionName);
+        if (method_exists($this, $functionName)) {
+            $this->$functionName();
+        
+        } else if(method_exists($this, "preventDefault")) {
+            $met = "preventDefault";
+            $this->$met();
+        
+        } else {
+            throw new BadFunctionCallException("Usecase not exists");
+        }
+    }
+
+    public function loadView(string $path, array $data = null, string $msg = null){
+        $caminho = __DIR__ . "/../views/" . $path;
+        // echo("msg=");
+        // print_r($msg);
+        if(file_exists($caminho)){
+             require $caminho;
+        } else {
+            print "Erro ao carregar a view";
+        }
+    }
+    function create(){
+    $user = new UserModel();
+
+	$user->setUsername($_POST["field_nome"]);
+	$user->setGenero($_POST["field_genero"]);
+    $user->setEscolaridade($_POST["field_escolaridade"]);
+	$user->setEmail($_POST["field_email"]);
+    $user->setPassword($_POST["field_password"]);   
 
     $userRepository = new UserRepository();
 
     $id = $userRepository->create($user);
-    print_r($userRepository);
+    //print_r($userRepository);
     //findAll();
 }
 
@@ -32,7 +77,7 @@ function findAll()
     $data['titulo'] = "listar usuarios";
     $data['usuarios'] = $usuarios;
 
-    Controller::loadView("users/list.php", $data);
+    $this->loadView("users/list.php", $data);
 }
 
 function findUserById()
@@ -41,12 +86,13 @@ function findUserById()
     print_r($idParam);
     $userRepository = new UserRepository();
     $usuario = $userRepository->findUserById($idParam);
-
     print "<pre>";
     print_r($usuario);
     print "</pre>";
+    //$data= $usuario;
+    $this->loadView("users/perfil.php");
 
-    //Controller::loadView("users/list.php");
+
 }
 
 function deleteUserById()
@@ -58,8 +104,17 @@ function deleteUserById()
 
     //Controller::loadView("users/list.php", $data);
 }
+private function edit(){
+    $idParam = $_GET['id'];
+    $userRepository = new UserRepository(); 
+    $usuario = $userRepository->findUserById($idParam);
+    $data['usuarios'][0] = $usuario;
+
+    $this->loadView("../users/editUser.php", $data);
+}
 
 function preventDefault() {
     print "call preventDefault()";
     //Controller::loadView("users/list.php", $data);
+}
 }
