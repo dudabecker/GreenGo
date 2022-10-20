20fd3fee0f2556ace78e323ae8ec05b26ea70b75
nicollytavassi <nicollytavassii@gmail.com>
2022-10-20 19:09:07 -0300

    arrumei os css's

--- app/views/login.php
+++ app/views/login.php
@@ -7,8 +7,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../public/styles.css">
-    <link rel="stylesheet" type="text/css" href="../public/styles-form.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/styles-form.css">
 
     <title>Entrar</title>
 </head>
