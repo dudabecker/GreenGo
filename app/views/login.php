4767933735b6f9871fa01037f290b7a0620fe32d
nicollytavassi <nicollytavassii@gmail.com>
2022-11-30 07:53:44 -0300

    nao lembro o qeu eu fiz aqui

--- app/views/login.php
+++ app/views/login.php
@@ -9,6 +9,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link rel="stylesheet" type="text/css" href="css/styles-form.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <!--scripts-->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+    crossorigin="anonymous"></script>
+  <!-- Progress bar -->
+  <script src="js/progressbar.min.js"></script>
+  <!-- Parallax -->
+  <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
 
     <title>Entrar</title>
 </head>
@@ -18,6 +34,8 @@
     <img class="img-top" src="../public/imagem-top.svg">
 
     <div class="container">
+        <div class="row">
+            <div class="col-md-center">
         <form class="form-login" id="loginform" name="loginform" method="post" action="../controllers/loginController.php?action=logar">
 
             <h2 class="titulo-form">
@@ -54,8 +72,10 @@
         </form>
     </div>
     <br>
-
-    <img class="img-bottom" src="../public/imagem-bottom.svg">
+                <div class="img-fluid">
+                    <img class="img-bottom" src="../public/imagem-bottom.svg">
+                </div>
+   
     
 </body>
 
