dbc041cf8d9c1b49e5b8597b15411d4eb50e3c48
Alana Brandao <oliveira.alanabrandao@gmail.com>
2022-12-14 17:25:30 -0300

    cabecalhos prontos para serem copiados!

---
+++ app/views/cabecalho.php
@@ -0,0 +1,132 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COD DOS CABECALHOS</title>
+
+    <!--FAVICON-->
+    <link rel="icon" href="../public/favicon.svg">
+
+    <!-- Fonte -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap" rel="stylesheet">
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/cabecalho-index.css">
+
+    <!--scripts-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+
+    <!-- Progress bar -->
+    <script src="js/progressbar.min.js"></script>
+    <!-- Parallax -->
+    <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
+</head>
+
+<body>
+
+
+    <nav id="indexinteiro">
+
+        <div class="col-xs-12" id="nav-container">
+
+            <div id="itensmenu">
+
+                <nav class="navbar navbar-expand-lg " id="menu">
+                    <a href="index.html" class="nav-brand">
+                        <div class="row justify-content-md-left">
+                            <div id="imgmenu">
+                               <img class="img-responsive" id="logo" src="" alt="">
+                            </div>
+                        </div>
+                    </a>
+
+
+                    <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
+                        <div class="navbar-nav" id="navbar-links">
+
+                            <a class="nav-item nav-link" id="portfolio-menu" href="../views/projeto.html"> Projeto </a>
+                            <a class="nav-item nav-link" id="mapa-menu" href="#"> Mapa</a>
+                            <a class="nav-item nav-link" id="jogar-menu" href="#"> Jogar </a>
+                            <a class="nav-item nav-link" id="registro-menu" href="#"> Registro </a>
+                            <a class="nav-item nav-link" id="quiz-menu" href="#"> Quiz </a>
+                            <a class="nav-item nav-link" id="equipes-menu" href="#"> Equipes </a>
+                            <a class="nav-item nav-link" id="botaoentrar" href="#"> Adm </a>
+                        </div>
+                    </div>
+
+                </nav>
+            </div>
+        </div>
+    </nav>
+
+    <nav>
+        <div class="col-xs-12" id="nav-container">
+
+            <div id="itensmenu">
+
+                <nav class="navbar navbar-expand-lg " id="menu">
+                    <a href="index.html" class="nav-brand">
+                        <div class="row justify-content-md-left">
+                            <div id="imgmenu">
+                                <img class="img-responsive" id="logo" >
+                            </div>
+                        </div>
+                    </a>
+
+                    <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
+                        <div class="navbar-nav" id="navbar-links">
+
+                            <a class="nav-item nav-link" id="portfolio-menu" href="../views/projeto.html"> Projeto </a>
+                            <a class="nav-item nav-link" id="registro-menu" href="#"> Mapa</a>
+                            <a class="nav-item nav-link" id="identificar-menu" href="#"> Jogar </a>
+                            <a class="nav-item nav-link" id="botaoentrar" href="perfil.php"> Eu </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </nav>
+
+    <nav id="indexinteiro">
+
+        <div class="col-xs-12" id="nav-container">
+
+            <div id="itensmenu">
+
+                <nav class="navbar navbar-expand-lg " id="menu">
+                    <a href="index.html" class="nav-brand">
+                        <div class="row justify-content-md-left">
+                            <div id="imgmenu">
+                            <img class="img-responsive" id="logo" >
+                            </div>
+                        </div>
+                    </a>
+
+                    <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
+                        <div class="navbar-nav" id="navbar-links">
+
+                            <a class="nav-item nav-link" id="portfolio-menu" href="../views/projeto.html"> Projeto </a>
+                            <a class="nav-item nav-link" id="registro-menu" href="#"> Mapa</a>
+                            <a class="nav-item nav-link" id="identificar-menu" href="#"> Jogar </a>
+                            <a class="nav-item nav-link" id="botaoentrar" href="../views/login.php"> Entrar </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </nav>
