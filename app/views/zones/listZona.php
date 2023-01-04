dedf5cce98ce66f6997118acd387963f1a72a281
Maria Eduarda Heck <54290646+Mariaehsb@users.noreply.github.com>
2023-01-04 19:39:03 -0300

    qnt plantas meramente ilustrativo

--- app/views/zones/listZona.php
+++ app/views/zones/listZona.php
@@ -1,48 +1,120 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zonas</title>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+    <!--FAVICON-->
+    <link rel="icon" href="../public/favicon.svg">
+    <!-- Fonte -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap" rel="stylesheet">
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="../views/css/perfil.css">
+    <!--scripts-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+    <!-- Progress bar -->
+    <script src="js/progressbar.min.js"></script>
+    <!-- Parallax -->
+    <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
+
 </head>
-<body>
-
-    <div class="container">
-
-    <h1>Zonas</h1>
 
 
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Quantidade de plantas</th>
-      <th scope="col">opcoes</th>
-    </tr>
-  </thead>
-  <tbody>
-  
-  <?php foreach($data['zonas'] as $zona): ?>
+<nav>
+    <div class="col-xs-12" id="nav-container">
 
-    <tr>
-      <th scope="row"><?= $zona -> getIdZona() ?></th>
-      <td><?= $zona->getQntPlantas() ?> </td>
-      <td><a href="<?= $zona->findZonaByIdZona($idParam) ?>"> mostrar zona </a></td>
-    </tr>
+        <div id="itensmenu">
 
-    <?php endforeach; ?>
-    
-  </tbody>
-</table>
+            <nav class="navbar navbar-expand-lg " id="menu">
+                <a href="index.html" class="nav-brand">
+                    <div class="row justify-content-md-left">
+                        <div id="imgmenu">
+                            <img class="img-responsive" src="../public/icone 3.svg" alt="">
+                        </div>
+                    </div>
+                </a>
 
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links"
+                    aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
+                    <div class="navbar-nav" id="navbar-links">
 
-
-
-
+                        <a class="nav-item nav-link" id="portfolio-menu" href="../projeto.html"> Projeto </a>
+                        <a class="nav-item nav-link" id="registro-menu"> Mapa</a>
+                        <a class="nav-item nav-link" id="identificar-menu"> Jogar </a>
+                        <a class="nav-item nav-link" id="botaoentrar" href="perfil.php"> Eu </a>
+                    </div>
+                </div>
+            </nav>
+        </div>
     </div>
+</nav>
 
+<body>
+    <div class="container">
+        <h2 class="titulo">
+            Zonas
+        </h2>
+
+
+        <?php foreach($data['zonas'] as $zona): ?>
+
+            <div class="row justify-content-md-left">
+            <div class="row">
+        <p>
+           
+           
+            Identificação : <?= $zona['idZona'] ?><br>
+            Nome: <?= $zona['nomeZona'] ?><br>
+            quantidade de plantas <?= $zona['nomeZona'] ?><br>
+            Pontuação Minima: <?= $zona['pontoZona'] ?><br>
+            
+            [ <a href="./ZonaController.php?action=edit&idZona=<?= $zona['idZona'] ?>">Editar</a> ]
+            [ <a href="./ZonaController.php?action=deleteZonaById&idZona=<?= $zona['idZona'] ?>">Excluir</a> ]
+        </p>
+
+        <div class="row justify-content-md-left">
+            <div class="row">
+                <!--EDITAR-->
+                <div class="col">
+                 
+                </div>
+
+                <!--EXCLUIR-->
+                <div class="col">
+                    
+                </div>
+
+                <div class="col">
+                 
+                </div>
+                <div class="col">
+                 
+                </div>
+                
+            </div>
+        </div>
+        
+    </div>
 </body>
+
 </html>
+
+<?php endforeach; ?>
+<div>
+[ <a href="../views/zones/definirZona.php">Cadastrar zona</a> ]
+                </div>
