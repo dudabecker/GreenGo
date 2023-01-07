b2292b47ace017f81297f225c392ab4dd15c86d3
nicollytavassi <nicollytavassii@gmail.com>
2023-01-07 10:11:51 -0300

    tinha esquecido de colocar registroplanta na pasta plantas
    nao sabia dessa pasta e acabei fazendo uma pagina inteira para cadastro de individuo sem saber que ja tinha uma :+1:

--- app/views/plantas/registroplanta.php
+++ app/views/plantas/registroplanta.php
@@ -36,7 +36,7 @@
     <script src="js/progressbar.min.js"></script>
     <!-- Parallax -->
     <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
-    <link rel="stylesheet" href="css/registroplanta.css">
+    <link rel="stylesheet" href="../css/registroplanta.css">
     <script src="js/registro.js"></script>
     <link rel="stylesheet" href="js/registro.js">
     <link rel="stylesheet" href="css/editorwys.css" type="text/css" media="all" />
@@ -199,7 +199,7 @@
                 <a id="textodescritivo">História</a>
                 <textarea id="txtconteudo" name="descricao"></textarea>
 
-                <script src="ckeditor/build/ckeditor.js"></script>
+                <script src="../ckeditor/build/ckeditor.js"></script>
                 <script>ClassicEditor
                         .create(document.querySelector('#txtconteudo'), {
                             licenseKey: '',
