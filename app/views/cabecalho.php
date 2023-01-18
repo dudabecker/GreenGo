355d75c6e937e95b9de9bf601e7704f47b3884b2
nicollytavassi <nicollytavassii@gmail.com>
2023-01-18 16:38:19 -0300

    arrumei os cabecalhos

--- app/views/cabecalho.php
+++ app/views/cabecalho.php
@@ -18,6 +18,15 @@
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <link rel="stylesheet" href="css/cabecalho.css">
+    <!--scripts-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -30,14 +39,19 @@
                 <nav class="navbar navbar-expand-lg " id="menu">
                     <a href="../views/index.html" class="nav-brand">
                         <div class="row justify-content-md-left">
-                            <div id="imgmenu">
-                               <img class="img-responsive" id="logo" src="" alt="">
-                            </div>
-                        </div>
-                    </a>
+                            
+                            <img class="img-responsive" src="../public/logo-green.svg"  id="logo" >
+                        
+                    </div>
+                </a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links"
+                    aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"> <img src="../public/menu.svg" id="menuicon"></span>
+                </button>
 
                     <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
-                        <div class="navbar-nav" id="navbar-links">
+                        <div class="navbar-nav" >
 
                             <a class="nav-item nav-link" id="portfolio-menu" href="../views/projeto.html"> Projeto </a>
                             <a class="nav-item nav-link" id="mapa-menu" href="#"> Mapa</a>
@@ -63,13 +77,17 @@
                     <a href="../views/index.html" class="nav-brand">
                         <div class="row justify-content-md-left">
                             <div id="imgmenu">
-                                <img class="img-responsive" id="logo" >
-                            </div>
+                            <img class="img-responsive" src="../public/logo-green.svg"  id="logo" >
                         </div>
-                    </a>
+                    </div>
+                </a>
 
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links"
+                    aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"> <img src="../public/menu.svg" id="menuicon"></span>
+                </button>
                     <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
-                        <div class="navbar-nav" id="navbar-links">
+                        <div class="navbar-nav" >
 
                             <a class="nav-item nav-link" id="portfolio-menu" href="../views/projeto.html"> Projeto </a>
                             <a class="nav-item nav-link" id="registro-menu" href="#"> Mapa</a>
@@ -91,13 +109,18 @@
                     <a href="../views/index.html" class="nav-brand">
                         <div class="row justify-content-md-left">
                             <div id="imgmenu">
-                            <img class="img-responsive" id="logo" >
-                            </div>
+                            <img class="img-responsive" src="../public/logo-green.svg"  id="logo" >
                         </div>
-                    </a>
+                    </div>
+                </a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links"
+                    aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"> <img src="../public/menu.svg" id="menuicon"></span>
+                </button>
 
                     <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
-                        <div class="navbar-nav" id="navbar-links">
+                        <div class="navbar-nav" >
 
                             <a class="nav-item nav-link" id="portfolio-menu" href="../views/projeto.html"> Projeto </a>
                             <a class="nav-item nav-link" id="registro-menu" href="#"> Mapa</a>
--- app/views/cabecalhoVisitante.php
+++ app/views/cabecalhoVisitante.php
@@ -7,10 +7,15 @@
                 <a href="../views/index.html" class="nav-brand">
                     <div class="row justify-content-md-left">
                         <div id="imgmenu">
-                        <img class="img-responsive" id="logo" >
+                        <img class="img-responsive" src="../public/logo-green.svg"  id="logo" >
                         </div>
                     </div>
                 </a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links"
+                    aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"> <img src="../public/menu.svg" id="menuicon"></span>
+                </button>
 
                 <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
                     <div class="navbar-nav" id="navbar-links">
