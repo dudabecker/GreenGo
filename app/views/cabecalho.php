e7a25b0f91d27c7242fbecad6277ed2a72817039
Alana Brandao <oliveira.alanabrandao@gmail.com>
2023-01-25 12:50:08 -0300

    Update cabecalho.php

--- app/views/cabecalho.php
+++ app/views/cabecalho.php
@@ -33,15 +33,15 @@
 
 <!--------------ADMIN-------------->
 
-    <nav>
-        <div class="col-xs-12" id="nav-container">
-            <div id="itensmenu">
-                <nav class="navbar navbar-expand-lg " id="menu">
-                    <a href="../views/index.html" class="nav-brand">
-                        <div class="row justify-content-md-left">
-                            
-                            <img class="img-responsive" src="../public/logo-green.svg"  id="logo" >
-                        
+<nav>
+    <div class="col-xs-12" id="nav-container">
+        <div id="itensmenu">
+            <nav class="navbar navbar-expand-lg " id="menu">
+                <a href="../views/index.html" class="nav-brand">
+                    <div class="row justify-content-md-left">
+                        <div id="imgmenu">
+                        <img class="img-responsive" src="../public/logo-green.svg"  id="logo" >
+                        </div>
                     </div>
                 </a>
 
@@ -50,23 +50,23 @@
                     <span class="navbar-toggler-icon"> <img src="../public/menu.svg" id="menuicon"></span>
                 </button>
 
-                    <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
-                        <div class="navbar-nav" >
+                <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
+                    <div class="navbar-nav" >
 
-                            <a class="nav-item nav-link" id="portfolio-menu" href="../views/projeto.html"> Projeto </a>
-                            <a class="nav-item nav-link" id="mapa-menu" href="#"> Mapa</a>
-                            <a class="nav-item nav-link" id="jogar-menu" href="#"> Jogar </a>
-                            <a class="nav-item nav-link" id="registro-menu" href="#"> Registro </a>
-                            <a class="nav-item nav-link" id="quiz-menu" href="#"> Quiz </a>
-                            <a class="nav-item nav-link" id="equipes-menu" href="#"> Equipes </a>
-                            <a class="nav-item nav-link" id="botaoentrar" href="#"> Adm </a>
-                        </div>
+                        <a class="nav-item nav-link" id="projeto-menu" href="#"> Projeto </a>
+                        <a class="nav-item nav-link" id="mapa-menu" href="#"> Mapa</a>
+                        <a class="nav-item nav-link" id="jogar-menu" href="#"> Jogar </a>
+                        <a class="nav-item nav-link" id="zonas-menu" href="#"> Zonas </a>
+                        <a class="nav-item nav-link" id="especies-menu" href="#"> Espécies </a>
+                        <a class="nav-item nav-link" id="usuarios-menu" href="#"> Usuários </a>
+                        <a class="nav-item nav-link" id="botaoentrar" href="#"> Adm </a>
                     </div>
+                </div>
 
-                </nav>
-            </div>
+            </nav>
         </div>
-    </nav>
+    </div>
+</nav>
 
 <!--------------JOGADOR-------------->
 
