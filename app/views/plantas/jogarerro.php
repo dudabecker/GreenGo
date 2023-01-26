ecea56e14b3af0740f8a0220517b0f62dcc7c7fd
nicollytavassi <nicollytavassii@gmail.com>
2023-01-26 20:52:56 -0300

    jogar erro.php

---
+++ app/views/plantas/jogarerro.php
@@ -0,0 +1,92 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jogar erro</title>
+
+    <!--FAVICON-->
+    <link rel="icon" href="../public/favicon.svg">
+    <!-- Fonte -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap" rel="stylesheet">
+    <!--BOOTSTRAP-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/identificarplantas.css">
+    <!--scripts-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+    <!-- Progress bar -->
+    <script src="js/progressbar.min.js"></script>
+    <!-- Parallax -->
+    <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
+
+</head>
+
+<nav>
+    <div class="col-xs-12" id="nav-container">
+
+        <div id="itensmenu">
+
+            <nav class="navbar navbar-expand-lg " id="menu">
+                <a href="index.html" class="nav-brand">
+                    <div class="row justify-content-md-left">
+                        <div id="imgmenu">
+                            <img class="img-responsive" src="../public/icone 3.svg" alt="">
+                        </div>
+                    </div>
+                </a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links"
+                    aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
+                    <div class="navbar-nav" id="navbar-links">
+
+                    <a class="nav-item nav-link" id="itemmenu" href="projeto.html"> Projeto </a>
+                        <a class="nav-item nav-link" id="itemmenu" href="./mapa.php"> Mapa</a>
+                        <a class="nav-item nav-link" id="itemmenu" href="../Controllers/PlantaController.php?action=formIdentificarPlanta"> Jogar </a>
+                        <a class="nav-item nav-link" id="botaoentrar" href="perfil.php"> Eu </a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+</nav>
+
+<body>
+
+
+        <div class="container">
+
+            <h2 id="textoplanta">Calma aí!</h2>
+        
+
+<form action="./PlantaController.php?action=identificarPlanta" method="POST">
+	<a id="a"> Não conhecemos essa planta ainda :(</a><br>
+    <a id="a"> Tente de novo.</a>
+    
+     <input type="text" class="form-control" name="codNumerico" id="codigo" >
+	<br>
+    <!--Código QR: <input type="text" name="codQR">-->
+	<br>
+
+	<input type="submit" value="Próximo" id="botaoproximo">
+
+    
+                    
+               
+</form>		
+        <a class="pontuacao" id="a2"> Sua pontuação atual é de <!--GET PONTUACAO DO USUARIO--> xxxx pontos</a>
+</div>
+</body>
+</html>
