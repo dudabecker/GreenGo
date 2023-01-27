1f6744d61b833439abbb7cbb26f5728324fab8ff
Maria Eduarda Heck <54290646+Mariaehsb@users.noreply.github.com>
2023-01-27 14:11:43 -0300

    links ok

--- app/views/cabecalhoJogador.php
+++ app/views/cabecalhoJogador.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -47,13 +46,14 @@
                     aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"> <img src="../public/menu.svg" id="menuicon"></span>
                 </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
-                    <div class="navbar-nav" >
 
-                        <a class="nav-item nav-link" id="projeto-menu" href="#"> Projeto </a>
-                        <a class="nav-item nav-link" id="mapa-menu" href="#"> Mapa</a>
-                        <a class="nav-item nav-link" id="identificar-menu" href="#"> Jogar </a>
-                        <a class="nav-item nav-link" id="botaoentrar" href="#"> Eu </a>
+                <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
+                    <div class="navbar-nav">
+
+                    <a class="nav-item nav-link" id="projeto-menu" href="../views/projeto.html"> Projeto </a>
+                    <a class="nav-item nav-link" id="mapa-menu" href="../views/mapa.php"> Mapa</a>
+                    <a class="nav-item nav-link" id="itemmenu" href="./PlantaController.php?action=formIdentificarPlanta"> Jogar </a>
+                    <a class="nav-item nav-link" id="botaoentrar" href="./UserController.php?action=findUserById"> Eu </a>
                     </div>
                 </div>
             </nav>
