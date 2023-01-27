ee7c06912b2d5a2209619567dae665891782b022
Maria Eduarda Heck <54290646+Mariaehsb@users.noreply.github.com>
2023-01-27 14:13:06 -0300

    links ok

--- app/views/cabecalhoVisitante.php
+++ app/views/cabecalhoVisitante.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -51,10 +50,10 @@
                 <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
                     <div class="navbar-nav">
 
-                        <a class="nav-item nav-link" id="projeto-menu" href="#"> Projeto </a>
-                        <a class="nav-item nav-link" id="mapa-menu" href="#"> Mapa</a>
-                        <a class="nav-item nav-link" id="identificar-menu" href="#"> Jogar </a>
-                        <a class="nav-item nav-link" id="botaoentrar" href="#"> Entrar </a>
+                    <a class="nav-item nav-link" id="projeto-menu" href="../views/projeto.html"> Projeto </a>
+                    <a class="nav-item nav-link" id="mapa-menu" href="../views/mapa.php"> Mapa</a>
+                    <a class="nav-item nav-link" id="itemmenu" href="./PlantaController.php?action=formIdentificarPlanta"> Jogar </a>
+                    <a class="nav-item nav-link" id="botaoentrar" href="../views/users/login.php"> Entrar </a>
                     </div>
                 </div>
             </nav>
