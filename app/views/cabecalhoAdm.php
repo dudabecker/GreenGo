b7a8d95607cb47c695470c9e02a1b2a3a45179c5
Alana Brandao <oliveira.alanabrandao@gmail.com>
2023-01-30 00:42:57 -0300

    link do mapa nos cabecalhos

--- app/views/cabecalhoAdm.php
+++ app/views/cabecalhoAdm.php
@@ -51,8 +51,8 @@
                 <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
                     <div class="navbar-nav">
 
-                        <a class="nav-item nav-link" id="projeto-menu" href="../views/projeto.html"> Projeto </a>
-                        <a class="nav-item nav-link" id="mapa-menu" href="./views/mapa.php"> Mapa</a>
+                        <a class="nav-item nav-link" id="projeto-menu" href="../views/projeto.php"> Projeto </a>
+                        <a class="nav-item nav-link" id="mapa-menu" href="..\controllers\EspecieController.php?action=EspeciesMapa"> Mapa</a>
                         <a class="nav-item nav-link" id="itemmenu" href="./PlantaController.php?action=formIdentificarPlanta"> Jogar </a>
                         <a class="nav-item nav-link" id="zonas-menu" href="./ZonaController.php?action=findZonas"> Zonas </a>
                         <a class="nav-item nav-link" id="especies-menu" href="./EspecieController.php?action=findAll"> Espécies </a>
--- app/views/cabecalhoJogador.php
+++ app/views/cabecalhoJogador.php
@@ -50,8 +50,8 @@
                 <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
                     <div class="navbar-nav">
 
-                    <a class="nav-item nav-link" id="projeto-menu" href="../views/projeto.html"> Projeto </a>
-                    <a class="nav-item nav-link" id="mapa-menu" href="../views/mapa.php"> Mapa</a>
+                    <a class="nav-item nav-link" id="projeto-menu" href="../views/projeto.php"> Projeto </a>
+                    <a class="nav-item nav-link" id="mapa-menu" href="..\controllers\EspecieController.php?action=EspeciesMapa"> Mapa</a>
                     <a class="nav-item nav-link" id="itemmenu" href="./PlantaController.php?action=formIdentificarPlanta"> Jogar </a>
                     <a class="nav-item nav-link" id="botaoentrar" href="./UserController.php?action=findUserById"> Eu </a>
                     </div>
